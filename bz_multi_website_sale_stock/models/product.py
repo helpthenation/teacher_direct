7207fb0750799bf2a09bf0861a5773096c2d5547
KrisQ <kquemener@yahoo.com>
2018-03-07 08:57:27 -0600

    Try again

--- bz_multi_website_sale_stock/models/product.py
+++ bz_multi_website_sale_stock/models/product.py
@@ -5,7 +5,6 @@ from odoo import models, fields, api
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-
     def _compute_quantities_dict(self, lot_id, owner_id, package_id, from_date=False, to_date=False):
         '''
         Get quantity based on current website warehouse
